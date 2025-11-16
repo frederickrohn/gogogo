e4c82adfbd5f54dc3d75adb95b5246b55dec184d
Frederick Rohn <fnr2009@nyu.edu>
2025-11-16 13:52:48 -0800

    struct

--- main.go
+++ main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+type User struct {
+	ID int
+	Name string
+}
+
+func greet(u User) string{
+	str := "Hi! I'm " + u.Name
+	return str
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+
+	user:= User{
+		ID: 1,
+		Name: "Fred",
+	}
+	greeting:= greet(user)
+
+	fmt.Println(greeting)
 }
